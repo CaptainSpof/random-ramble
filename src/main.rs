53021e204d6cd9f4f1836caa68a1b80585493af3
Cédric Da Fonseca <dafonseca.cedric@gmail.com>
2020-01-28 19:05:28 +0100

    Use relative path for adjectives and themes

--- src/main.rs
+++ src/main.rs
@@ -6,62 +6,54 @@ mod config;
 use config::Config;
 
 use rand::seq::SliceRandom;
-use std::{
-    env,
-    io::{prelude::*, BufReader},
-};
+use std::io::{prelude::*, BufReader};
 
 fn main() {
     let config: Config = Config::from_args();
     // println!("{:#?}", config);
 
-    let home = env::var("HOME").expect("Unable to get HOME environment variable");
-    let adjs: Vec<String> = WalkDir::new(format!(
-        "{}/Projects/Rust/random-ramble/dict/adjectives",
-        home
-    ))
-    .into_iter()
-    .filter_map(|e| e.ok())
-    .filter(|metadata| metadata.file_type().is_file())
-    .filter(|file| {
-        config
-            .adjectives
-            .contains(&file.file_name().to_str().unwrap().to_string())
-    })
-    .flat_map(|f| {
-        let file = std::fs::File::open(f.path()).expect("Unable to open file");
-        let buf = BufReader::new(file);
-        buf.lines()
-            .map(|l| l.expect("Could not parse line"))
-            .filter(|l| match config.starts_with {
-                Some(ref p) => l.starts_with(p),
-                None => true,
-            })
-            .collect::<Vec<String>>()
-    })
-    .collect();
+    let adjs: Vec<String> = WalkDir::new("./dict/adjectives")
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|metadata| metadata.file_type().is_file())
+        .filter(|file| {
+            config
+                .adjectives
+                .contains(&file.file_name().to_str().unwrap().to_string())
+        })
+        .flat_map(|f| {
+            let file = std::fs::File::open(f.path()).expect("Unable to open file");
+            let buf = BufReader::new(file);
+            buf.lines()
+                .map(|l| l.expect("Could not parse line"))
+                .filter(|l| match config.starts_with {
+                    Some(ref p) => l.starts_with(p),
+                    None => true,
+                })
+                .collect::<Vec<String>>()
+        })
+        .collect();
 
-    let themes: Vec<String> =
-        WalkDir::new(format!("{}/Projects/Rust/random-ramble/dict/themes", home))
-            .into_iter()
-            .filter_map(|e| e.ok())
-            .filter(|metadata| metadata.file_type().is_file())
-            .filter(|file| match &config.themes {
-                Some(themes) => themes.contains(&file.file_name().to_str().unwrap().to_string()),
-                None => true,
-            })
-            .flat_map(|f| {
-                let file = std::fs::File::open(f.path()).expect("Unable to open file");
-                let buf = BufReader::new(file);
-                buf.lines()
-                    .map(|l| l.expect("Could not parse line"))
-                    .filter(|l| match config.starts_with {
-                        Some(ref p) => l.starts_with(p),
-                        None => true,
-                    })
-                    .collect::<Vec<String>>()
-            })
-            .collect();
+    let themes: Vec<String> = WalkDir::new("./dict/themes")
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|metadata| metadata.file_type().is_file())
+        .filter(|file| match &config.themes {
+            Some(themes) => themes.contains(&file.file_name().to_str().unwrap().to_string()),
+            None => true,
+        })
+        .flat_map(|f| {
+            let file = std::fs::File::open(f.path()).expect("Unable to open file");
+            let buf = BufReader::new(file);
+            buf.lines()
+                .map(|l| l.expect("Could not parse line"))
+                .filter(|l| match config.starts_with {
+                    Some(ref p) => l.starts_with(p),
+                    None => true,
+                })
+                .collect::<Vec<String>>()
+        })
+        .collect();
 
     let adj_random_sel: Vec<_> = adjs
         .choose_multiple(&mut rand::thread_rng(), config.list_length as usize)
