b3147bb788805a286524e2c3f7b379131df20302
CaptainSpof <55411256+CaptainSpof@users.noreply.github.com>
2021-08-11 19:21:58 +0200

    Refactor/hashmap (#13)
    feat(core): implem HashMap in `RandomRamble`
    
    `RandomRamble` now have a HashMap<RambleKind, Value> field
    `_ramble` (temporary).
    Also added a custom Filter for getting a random value from the `Context`.

--- core/tests/init.rs
+++ core/tests/init.rs
@@ -1,209 +1,234 @@
-use std::path::PathBuf;
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
 
-use random_ramble::refactor::{Ramble, RambleKind, RandomRamble};
+    use maplit::hashmap;
+    use random_ramble::refactor::{Ramble, RambleKind, RandomRamble};
 
-#[test]
-fn init_default() {
-    let rr = RandomRamble::default();
+    #[test]
+    fn init_default() {
+        let rr = RandomRamble::default();
 
-    assert_eq!(
-        rr,
-        RandomRamble {
-            rambles: vec![],
-            template: None
-        }
-    );
-}
+        assert_eq!(
+            rr,
+            RandomRamble {
+                rambles: vec![],
+                _rambles: HashMap::new(),
+                template: None,
+                context: None,
+            }
+        );
+    }
 
-#[test]
-fn init_with_adjs() {
-    let adjs = vec![
-        Ramble::new("Happy").with_kind(RambleKind::Adjective),
-        Ramble::new("Sad").with_kind(RambleKind::Adjective),
-    ];
+    #[test]
+    fn init_with_adjs() {
+        let adjs = vec![
+            Ramble::new("Happy").with_kind(RambleKind::Adjective),
+            Ramble::new("Sad").with_kind(RambleKind::Adjective),
+        ];
 
-    let rr = RandomRamble::new().with_adjs(adjs);
+        let rr = RandomRamble::new().with_adjs(adjs);
 
-    assert_eq!(
-        rr,
-        RandomRamble {
-            rambles: vec![
-                Ramble {
-                    kind: RambleKind::Adjective,
-                    value: "Happy",
-                    file: None
+        assert_eq!(
+            rr,
+            RandomRamble {
+                rambles: vec![
+                    Ramble {
+                        kind: RambleKind::Adjective,
+                        value: "Happy",
+                        file: None
+                    },
+                    Ramble {
+                        kind: RambleKind::Adjective,
+                        value: "Sad",
+                        file: None
+                    },
+                ],
+                _rambles: hashmap! {
+                    RambleKind::Adjective.to_string() => vec!["Happy", "Sad"],
                 },
-                Ramble {
+                template: None,
+                context: None,
+            }
+        );
+    }
+
+    #[test]
+    fn init_with_adj_from_string() {
+        let adj = "Pretty";
+
+        let rr: RandomRamble = RandomRamble::default().with_adj(adj.into());
+
+        assert_eq!(
+            rr,
+            RandomRamble {
+                rambles: vec![Ramble {
                     kind: RambleKind::Adjective,
-                    value: "Sad",
+                    value: "Pretty",
                     file: None
+                }],
+                _rambles: hashmap! {
+                    RambleKind::Adjective.to_string() => vec!["Pretty"],
                 },
-            ],
-            template: None
-        }
-    );
-}
+                template: None,
+                context: None,
+            }
+        );
+    }
 
-#[test]
-fn init_with_adj_from_string() {
-    let adj = "Pretty";
+    #[test]
+    fn init_with_adjs_from_string() {
+        let adj1 = "Kind";
+        let adj2 = "Ruthless";
 
-    let rr: RandomRamble = RandomRamble::default().with_adj(adj.into());
+        let rr = RandomRamble::new().with_adjs(vec![adj1.into(), adj2.into()]);
 
-    assert_eq!(
-        rr,
-        RandomRamble {
-            rambles: vec![Ramble {
-                kind: RambleKind::Adjective,
-                value: "Pretty",
-                file: None
-            },],
-            template: None
-        }
-    );
-}
+        assert_eq!(
+            rr,
+            RandomRamble {
+                rambles: vec![
+                    Ramble {
+                        kind: RambleKind::Adjective,
+                        value: "Kind",
+                        file: None
+                    },
+                    Ramble {
+                        kind: RambleKind::Adjective,
+                        value: "Ruthless",
+                        file: None
+                    },
+                ],
+                _rambles: hashmap! {
+                    RambleKind::Adjective.to_string() => vec!["Kind", "Ruthless"],
+                },
+                template: None,
+                context: None,
+            }
+        );
+    }
 
-#[test]
-fn init_with_adjs_from_string() {
-    let adj1 = "Kind";
-    let adj2 = "Ruthless";
+    // #[test]
+    // fn init_with_adjs_from_path() {
 
-    let rr = RandomRamble::new().with_adjs(vec![adj1.into(), adj2.into()]);
+    //     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    //     path.push("resources/tests/adjectives/");
 
-    assert_eq!(
-        rr,
-        RandomRamble {
-            rambles: vec![
-                Ramble {
-                    kind: RambleKind::Adjective,
-                    value: "Kind",
+    //     let rr = RandomRamble::new()
+    //         .with_adjs_path(&path);
+
+    //     assert_eq!(rr, RandomRamble {
+    //         rambles: vec![
+    //             Ramble_ {
+    //                 kind: RambleKind_::Adjective,
+    //                 value: "Ugly",
+    //                 file: Some(File {
+    //                     name: "test2",
+    //                     path: format!("{}test2", path.clone().into_os_string().into_string().expect("🤷"))
+    //                 })
+    //             },
+    //             Ramble_ {
+    //                 kind: RambleKind_::Adjective,
+    //                 value: "Pretty",
+    //                 file: Some(File {
+    //                     name: "test1",
+    //                     path: format!("{}test1", path.clone().into_os_string().into_string().expect("🤷"))
+    //                 })
+    //             },
+    //         ],
+    //         template: None
+    //     });
+    // }
+
+    #[test]
+    fn init_with_themes() {
+        let themes = vec![Ramble::new("King").with_kind(RambleKind::Theme)];
+
+        let rr = RandomRamble::new().with_themes(themes);
+
+        assert_eq!(
+            rr,
+            RandomRamble {
+                rambles: vec![Ramble {
+                    kind: RambleKind::Theme,
+                    value: "King",
                     file: None
+                },],
+                _rambles: hashmap! {
+                    RambleKind::Theme.to_string() => vec!["King"],
                 },
-                Ramble {
-                    kind: RambleKind::Adjective,
-                    value: "Ruthless",
+                template: None,
+                context: None,
+            }
+        );
+    }
+
+    #[test]
+    fn init_with_theme_from_string() {
+        let theme = "Toto";
+
+        let rr = RandomRamble::new().with_theme(theme.into());
+
+        assert_eq!(
+            rr,
+            RandomRamble {
+                rambles: vec![Ramble {
+                    kind: RambleKind::Theme,
+                    value: "Toto",
                     file: None
+                },],
+                _rambles: hashmap! {
+                    RambleKind::Theme.to_string() => vec!["Toto"],
                 },
-            ],
-            template: None
-        }
-    );
-}
+                template: None,
+                context: None,
+            }
+        );
+    }
 
-// #[test]
-// fn init_with_adjs_from_path() {
+    // #[test]
+    // fn init_with_themes_from_path() {
 
-//     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-//     path.push("resources/tests/adjectives/");
+    //     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    //     path.push("resources/tests/themes/");
 
-//     let rr = RandomRamble::new()
-//         .with_adjs_path(&path);
+    //     let rr = RandomRamble::new()
+    //         .with_themes_path(&path);
 
-//     assert_eq!(rr, RandomRamble {
-//         rambles: vec![
-//             Ramble_ {
-//                 kind: RambleKind_::Adjective,
-//                 value: "Ugly",
-//                 file: Some(File {
-//                     name: "test2",
-//                     path: format!("{}test2", path.clone().into_os_string().into_string().expect("🤷"))
-//                 })
-//             },
-//             Ramble_ {
-//                 kind: RambleKind_::Adjective,
-//                 value: "Pretty",
-//                 file: Some(File {
-//                     name: "test1",
-//                     path: format!("{}test1", path.clone().into_os_string().into_string().expect("🤷"))
-//                 })
-//             },
-//         ],
-//         template: None
-//     });
-// }
+    //     assert_eq!(rr, RandomRamble {
+    //         rambles: vec![
+    //             Ramble_ {
+    //                 kind: RambleKind_::Theme,
+    //                 value: "Titi",
+    //                 file: Some(File {
+    //                     name: "test2",
+    //                     path: format!("{}test2", path.clone().into_os_string().into_string().expect("🤷"))
+    //                 })
+    //             },
+    //             Ramble_ {
+    //                 kind: RambleKind_::Theme,
+    //                 value: "Toto",
+    //                 file: Some(File {
+    //                     name: "test1",
+    //                     path: format!("{}test1", path.clone().into_os_string().into_string().expect("🤷"))
+    //                 })
+    //             },
+    //         ],
+    //         template: None
+    //     });
+    // }
 
-#[test]
-fn init_with_themes() {
+    #[test]
+    fn init_with_template() {
+        let rr = RandomRamble::new().with_template("A {{adj}} for {{theme}}");
 
-    let themes = vec![
-        Ramble::new("King")
-            .with_kind(RambleKind::Theme)
-    ];
-
-    let rr = RandomRamble::new()
-        .with_themes(themes);
-
-    assert_eq!(rr, RandomRamble{
-        rambles: vec![
-            Ramble {
-                kind: RambleKind::Theme,
-                value: "King",
-                file: None
-            },
-        ],
-        template: None
-    });
-}
-
-#[test]
-fn init_with_theme_from_string() {
-    let theme = "Toto";
-
-    let rr = RandomRamble::new()
-        .with_theme(theme.into());
-
-    assert_eq!(rr, RandomRamble{
-        rambles: vec![
-            Ramble {
-                kind: RambleKind::Theme,
-                value: "Toto",
-                file: None
-            },
-        ],
-        template: None
-    });
-}
-
-// #[test]
-// fn init_with_themes_from_path() {
-
-//     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-//     path.push("resources/tests/themes/");
-
-//     let rr = RandomRamble::new()
-//         .with_themes_path(&path);
-
-//     assert_eq!(rr, RandomRamble {
-//         rambles: vec![
-//             Ramble_ {
-//                 kind: RambleKind_::Theme,
-//                 value: "Titi",
-//                 file: Some(File {
-//                     name: "test2",
-//                     path: format!("{}test2", path.clone().into_os_string().into_string().expect("🤷"))
-//                 })
-//             },
-//             Ramble_ {
-//                 kind: RambleKind_::Theme,
-//                 value: "Toto",
-//                 file: Some(File {
-//                     name: "test1",
-//                     path: format!("{}test1", path.clone().into_os_string().into_string().expect("🤷"))
-//                 })
-//             },
-//         ],
-//         template: None
-//     });
-// }
-
-#[test]
-fn init_with_template() {
-
-    let rr = RandomRamble::new()
-        .with_template("A {{adj}} for {{theme}}");
-
-    assert_eq!(rr, RandomRamble{
-        rambles: vec![],
-        template: Some("A {{adj}} for {{theme}}")
-    });
+        assert_eq!(
+            rr,
+            RandomRamble {
+                rambles: vec![],
+                _rambles: HashMap::new(),
+                template: Some("A {{adj}} for {{theme}}"),
+                context: None,
+            }
+        );
+    }
 }
