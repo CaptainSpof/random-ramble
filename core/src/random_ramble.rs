e4c1716d156803ee2a08e9bd4b4ab2663d3f745d
CaptainSpof <captain.spof@gmail.com>
2021-08-12 12:11:20 +0200

    🔧 fix(core): reflect changes in `Rand` api

--- core/src/random_ramble.rs
+++ core/src/random_ramble.rs
@@ -41,7 +41,7 @@ pub mod refactor {
             .as_array()
             .expect("must provide values alongside random");
 
-        let rng = rand::thread_rng().gen_range(0, values.len());
+        let rng = rand::thread_rng().gen_range(0..values.len());
         let val = values[rng].to_owned();
 
         Ok(val)
