a38cdb0cace86c9a1f293954aababee90a6af838
CaptainSpof <captain.spof@gmail.com>
2021-08-13 19:44:57 +0200

    - ✨ feat(core): allow filter to take category arg
    The Map passed to the filter now takes a `HashMap<RambleKind<'a>,
    Vec<Ramble<'a>>>` which contains a field `category` and a vec of values.
    
    The filter itself takes an argument `c`, that allows us to filter result
    for a given category.
    
    Here's an example:
    ```
    {{ adj | rr }} {{ theme | rr(c='hero') }}
    ```
    
    - various refactor

--- core/tests/mod.rs
+++ core/tests/mod.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod test {
-    use random_ramble::refactor::RandomRamble;
+    use random_ramble::refactor::{Ramble, RambleKind, RandomRamble};
 
     #[test]
     fn template_replace() {
@@ -32,9 +32,7 @@ mod test {
         let adjs = vec!["Clever", "Stupid"];
         let themes = vec!["Titi", "Fifi"];
 
-        let r = RandomRamble::new()
-            .with_adjs(adjs)
-            .with_themes(themes);
+        let r = RandomRamble::new().with_adjs(adjs).with_themes(themes);
 
         // TODO: find better way to test randomness
         println!("{}", r.to_string());
@@ -53,7 +51,80 @@ mod test {
             .with_template("{{ adj | rr }} {{ emoji | rr }}");
 
         // TODO: find better way to test randomness
-        // assert_eq!(r, "Clever 🦀");
+        // assert_eq!(r.to_string(), "Clever 🦀");
         assert_eq!(r.to_string().len(), "Clever 🦀".len());
+    }
+
+    #[test]
+    fn template_replace_custom_ramble_vec_with_ramble() {
+        let en = vec!["Clever", "Stupid"];
+
+        let adjs = Ramble {
+            category: Some("en"),
+            values: en,
+        };
+
+        let emojis = vec!["🦀", "🐕"];
+
+        let r = RandomRamble::new()
+            .with_ramble(RambleKind::Adjective, adjs)
+            .with_others("emoji", emojis)
+            .with_template("{{ adj | rr }} {{ emoji | rr }}");
+
+        // TODO: find better way to test randomness
+        // assert_eq!(r.to_string(), "Clever 🦀");
+        assert_eq!(r.to_string().len(), "Clever 🦀".len());
+    }
+
+    #[test]
+    fn template_replace_custom_ramble_vec_with_category() {
+        let en = vec!["Clever", "Stupid"];
+
+        let adjs = Ramble {
+            category: Some("en"),
+            values: en,
+        };
+
+        let emojis = vec!["🦀", "🐕"];
+
+        let r = RandomRamble::new()
+            .with_ramble(RambleKind::Adjective, adjs)
+            .with_others("emoji", emojis)
+            .with_template("{{ adj | rr(c='en') }} {{ emoji | rr }}");
+
+        // TODO: find better way to test randomness
+        // assert_eq!(r.to_string(), "Clever 🦀");
+        assert_eq!(r.to_string().len(), "Clever 🦀".len());
+    }
+    #[test]
+    fn template_replace_custom_ramble_vec_with_categories() {
+        let en = vec!["Clever", "Stupid"];
+        let fr = vec!["Malin", "Idiot"];
+
+        let en_adjs = Ramble {
+            category: Some("en"),
+            values: en,
+        };
+        let fr_adjs = Ramble {
+            category: Some("fr"),
+            values: fr,
+        };
+
+        let emojis = vec!["🦀", "🐕"];
+
+        let r = RandomRamble::new()
+            .with_rambles(RambleKind::Adjective, vec![en_adjs, fr_adjs])
+            .with_others("emoji", emojis)
+            .with_template("{{ adj | rr(c='fr') }} {{ emoji | rr }}");
+
+        let r = r.to_string();
+        let fr = vec!["Malin", "Idiot"];
+        assert!(fr
+            .iter()
+            .any(|&a| a == r.split(' ').collect::<Vec<&str>>()[0]));
+
+        // TODO: find better way to test randomness
+        // assert_eq!(r.to_string(), "Idiot 🐕");
+        assert_eq!(r.len(), "Malin 🦀".len());
     }
 }
