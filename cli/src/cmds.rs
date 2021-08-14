b40ed85cf20fb9a2a3945be47abfd0ffd25f6637
CaptainSpof <captain.spof@gmail.com>
2021-08-14 13:51:18 +0200

    fix(clippy): fix clippy warnings

--- cli/src/cmds.rs
+++ cli/src/cmds.rs
@@ -1,9 +1,9 @@
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::{prelude::*, BufReader};
-use std::path::PathBuf;
+use std::path::Path;
 
-pub fn add(theme_path: &PathBuf, theme: &str, entries: Vec<String>) {
+pub fn add(theme_path: &Path, theme: &str, entries: Vec<String>) {
     debug!("trying to add: {:?} to {}", entries, theme);
 
     let file = OpenOptions::new()
@@ -39,7 +39,7 @@ pub fn add(theme_path: &PathBuf, theme: &str, entries: Vec<String>) {
     }
 }
 
-pub fn delete(theme_path: &PathBuf, theme: &str, entries: Vec<String>) {
+pub fn delete(theme_path: &Path, theme: &str, entries: Vec<String>) {
     debug!("trying to delete: {:?} to {}", entries, theme);
 
     let file = OpenOptions::new()
@@ -63,7 +63,7 @@ pub fn delete(theme_path: &PathBuf, theme: &str, entries: Vec<String>) {
                 .cloned()
                 .collect();
 
-            lines.retain(|l| !entries.contains(&l));
+            lines.retain(|l| !entries.contains(l));
 
             if !lines.is_empty() {
                 if !deleting.is_empty() {
