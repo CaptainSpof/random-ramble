83f6f6debda5f2740913c5c9989920a6868523ba
CaptainSpof <captain.spof@gmail.com>
2021-08-16 19:03:59 +0200

    ✨ feat(flake): reintroduce naersk
    nix can now build, run and create a docker image

--- flake.nix
+++ flake.nix
@@ -1,23 +1,52 @@
 {
-  description = "A devShell example";
+  description = "RandomRamble: Generate stupid things randomly.";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nmattia/naersk";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, naersk, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pname = "rr";
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
         rustc-version = "latest";
-        rust-linux = pkgs.rust-bin.stable.${rustc-version}.default;
-      in {
+        rust-linux = pkgs.rust-bin.nightly.${rustc-version}.default;
+        # Override the version used in naersk
+        naersk-lib = naersk.lib."${system}".override {
+          cargo = rust-linux;
+          rustc = rust-linux;
+        };
+      in rec {
+
+        # `nix build`
+        packages.random-ramble = naersk-lib.buildPackage {
+          inherit pname;
+          root = ./.;
+        };
+        defaultPackage = packages.random-ramble;
+
+        packages.dockerImage = pkgs.dockerTools.buildLayeredImage {
+          name = "${pname}";
+          contents = [ packages.random-ramble ];
+          config.Entrypoint = [ "${pname}" ];
+        };
+        # defaultPackage = packages.random-ramble;
+
+        # `nix run`
+        apps.random-ramble = flake-utils.lib.mkApp {
+          drv = packages.random-ramble;
+        };
+        defaultApp = apps.random-ramble;
+
+
+        # `nix develop`
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
+          nativeBuildInputs = with pkgs; [
             # dev
             rust-analyzer  # rust lsp
             cargo-outdated # show outdated rust deps
@@ -37,6 +66,11 @@
               figlet "${pname}" -f $(showfigfonts | rg '(\w+) :' -r '$1' | shuf -n 1) | lolcat
               [ ! -f ./target/debug/${pname} ] && cargo build ; ln -sf ./target/debug/${pname} rr
             '';
+
+          # docker = hostPkgs.dockerTools.streamLayeredImage {
+          #   name = "${pname}";
+          #   contents =
+          # };
         };
       });
 }
