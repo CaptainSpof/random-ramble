a30b199a4019f48f95e509061eba8aaa6c045fee
CaptainSpof <captain.spof@gmail.com>
2021-08-19 19:23:23 +0200

    ✨ feat(error): return own error type
    rename `Jabber` -> `RambleError`

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,9 @@
+//! Rambling randomly about stuff.
+//!
+//! The `random-ramble` crate provides an API to generates random patterns
+//! given a `Tera` template and some inputs to choose from.
+//!
+
 #[macro_use]
 extern crate log;
 
@@ -7,5 +13,5 @@ mod macros;
 mod error;
 mod random_ramble;
 
-pub use crate::error::Jabber;
+pub use crate::error::RambleError;
 pub use crate::random_ramble::{refactor, RandomRamble};
