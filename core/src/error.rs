6824e5b572518d5b88dbafaaf32615d164f27238
CaptainSpof <captain.spof@gmail.com>
2021-08-19 19:23:23 +0200

    ✨ feat(error): return own error type
    rename `Jabber` -> `RambleError`

--- core/src/error.rs
+++ core/src/error.rs
@@ -1,14 +1,19 @@
+//! Error types
+
 use std::io;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum Jabber {
-    #[error("Oh shit, here we go again…")]
+pub enum RambleError {
+    #[error("Ah shit, here we go again…")]
     Custom(String),
-    #[error("We ain't found shit!")]
+    #[error("std::io says: We ain't found shit!")]
     IO(#[from] io::Error),
-    #[error("Walkdir · We ain't found shit!")]
+    #[error("walkdir says: We ain't found shit!")]
     Walkdir(#[from] walkdir::Error),
-    #[error("We ain'")]
+    #[error("Can't terraform a blackhole.")]
     Tera(#[from] tera::Error),
 }
+
+/// Convenient wrapper around std::Result.
+pub type Result<T> = ::std::result::Result<T, RambleError>;
